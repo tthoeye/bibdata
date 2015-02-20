ad873a91d679f51660f95cd343e0cca6ca44c3e4
Thimo Thoeye <thimothoeye@gmail.com>
2015-02-20 15:08:37 +0100

    Generic record parsing + first steps towards extracting author data

--- src/bibdata/RecordHandler.java
+++ src/bibdata/RecordHandler.java
@@ -6,7 +6,9 @@
 package bibdata;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 import javax.swing.JFileChooser;
 import org.xml.sax.Attributes;
@@ -21,7 +23,44 @@ public class RecordHandler extends DefaultHandler {
 
     public final static int PARSEMODE_NONE = 0;
     public final static int PARSEMODE_RECORD = 1;
-    public final static int PARSEMODE_ATRRIBUTES = 2;
+    
+    public static final Map<String, String> KEYS;
+
+    static {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("_200a", "title");
+        map.put("_200b", "type");
+        map.put("_101a", "language");
+        map.put("_2104", "publisher");
+        map.put("_210d", "year_published");
+        map.put("_215a", "pages");
+        map.put("_010a", "ISBN");
+        map.put("_700a", "author_lastname");
+        map.put("_700b", "author_firstname");
+        map.put("_7004", "author_type");
+        map.put("_015a", "EAN");
+        map.put("_900t", "series_title");
+        map.put("_205a", "series_edition");
+        map.put("_631a", "keyword_adults");
+        map.put("_105a", "literarytype");
+        map.put("_637a", "category_adults");
+        map.put("_633a", "keyword_youth");
+        map.put("_636a", "category_youth");
+        map.put("_6924", "age");
+        map.put("_690a", "SISO");
+        map.put("_696a", "ZIZO");
+        map.put("_011a", "ISSN");
+        map.put("_635a", "keywords_libraries");
+        map.put("_694a", "SISO_libraries");
+        map.put("_015z", "EAN_wrong");
+        map.put("_634a", "category_music");
+        map.put("_639a", "keywords_local");
+        map.put("_010z", "ISBN_wrong");
+        map.put("_632a", "keywords_youth");
+        map.put("_6914", "AVI");
+        map.put("_011z", "ISSN_wrong");
+        KEYS = Collections.unmodifiableMap(map);
+    }
     
     private JFileChooser fc;
     
@@ -34,11 +73,15 @@ public class RecordHandler extends DefaultHandler {
     private HashMap<String, String> record;
     
     private int parsemode;
-    
+    private String parsekey;
+    private String cnt;
+
     public RecordHandler(File file) {  
         
         // hold your horses!
         this.parsemode = PARSEMODE_NONE;
+        this.parsekey = "NONE";
+        this.cnt = "";
         this.fc = new JFileChooser();
         // Locate file to save records
         fc.setDialogTitle("Save records CSV to:");
@@ -51,6 +94,7 @@ public class RecordHandler extends DefaultHandler {
             System.out.println("Cannot save file");
             return;
         }
+        
         
         // Locate file to save keywords
         fc.setDialogTitle("Save keywords CSV to:");
@@ -89,12 +133,10 @@ public class RecordHandler extends DefaultHandler {
    
     }
  
-    public void startElement(String namespaceURI,
-                         String localName,
-                         String qName, 
-                         Attributes atts)
+    public void startElement(String nsURI,String localName, String qName, Attributes atts)
     throws SAXException {
 
+        // RECOGNIZE RECORD START TAG
         if (localName == "BibDocumentsGent.Record") {
             
             String id = atts.getValue("Value");
@@ -106,21 +148,47 @@ public class RecordHandler extends DefaultHandler {
                 record.put("id", id);
                 return;
             }
-        } else if (parsemode == PARSEMODE_RECORD) {
-            switch (localName) {
-                case "_200a":
-                    record.put("title", record.get("title"));
-                    return;
-            }  
+        } 
+        
+        if (parsemode == PARSEMODE_RECORD) {
+            if (KEYS.keySet().contains(localName)) {
+                parsekey = KEYS.get(localName);
+                this.cnt = atts.getValue("Cnt");
+            }
+        }
+        
+        
+    }
+    
+    @Override
+    public void endElement(String uri, String localName, String qName) {
+        if (localName == "BibDocumentsGent.Record") {
+            this.parsemode = PARSEMODE_NONE;
+            System.out.println("Finishing record: " + record.get("title"));
         }
     }
     
-    public void endElement(String uri, 
-                        String localName, 
-                        String qName) {
-        if (localName == "BibDocumentsGent.Record") {
-            this.parsemode = PARSEMODE_NONE;
-            System.out.println("Printing record title: " + record.get("title"));
+    @Override
+    public void characters(char[] ch,
+                       int start,
+                       int length)
+                throws SAXException {
+        
+        String value = new String(ch, start, length); 
+        // CURRENTLY PARSING A RECORD, IDENTIFY RECORD PROPERTIES
+        if (parsemode == PARSEMODE_RECORD && parsekey != "NONE") {
+            
+            switch (this.parsekey) {
+                case "author":
+                    // Do some crazy stuff
+                    System.out.println("Adding author: " + value);
+                    break;
+                default:
+                    record.put(parsekey, value);
+                    break;
+                      
+            } 
+            this.parsekey = "NONE";
         }
     }
 }
